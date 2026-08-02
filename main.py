bd77b6de9b82718583d5972a7400b33070a42be6
SamarthDudyala <samarth.dudyala@gmail.com>
2026-08-02 05:38:52 -0700

    Initialize Main.py Libraries

---
+++ main.py
@@ -0,0 +1,14 @@
+#basic management libraries
+import sys
+import time
+
+#Mathematical Calculations based libraries
+import numpy
+import scipy
+import matplotlib
+
+#3D Graphics
+import vpython 
+
+#Data Management
+import json
